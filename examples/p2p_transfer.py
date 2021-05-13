13e91e36da0f893b05ead8b76c5e99c2010772c8
fikgol <hit.cs.lijun@gmail.com>
2021-05-13 12:22:28 +0800

    Update p2p transfer example by receipt identifier

--- examples/p2p_transfer.py
+++ examples/p2p_transfer.py
@@ -2,20 +2,21 @@ from starcoin import starcoin_types as types
 from starcoin import starcoin_stdlib as stdlib
 from starcoin import serde_types as st
 from starcoin.sdk import (utils, client, local_account, auth_key)
+from starcoin.sdk.receipt_identifier import ReceiptIdentifier
 from cryptography.hazmat.primitives.asymmetric.ed25519 import (
     Ed25519PrivateKey, Ed25519PublicKey)
 import time
 import typing
 
 
-def transfer(cli: client.Client, sender: local_account.LocalAccount, payee: str, amount: st.uint128, payee_auth_key=typing.Union[auth_key.AuthKey, None]):
+def transfer(cli: client.Client, sender: local_account.LocalAccount, receipt: str, amount: st.uint128):
     seq_num = cli.get_account_sequence(
         "0x"+sender.account_address.bcs_serialize().hex())
-    payee_account = utils.account_address(payee)
+    receipt = ReceiptIdentifier.decode(receipt)
     script = stdlib.encode_peer_to_peer_script_function(
         token_type=utils.currency_code("STC"),
-        payee=payee_account,
-        payee_auth_key=payee_auth_key,  # assert the payee address has been on chain
+        payee=receipt.account_address,
+        payee_auth_key=receipt.auth_key.data if receipt.auth_key is not None else b"",
         amount=amount,
     )
     node_info = cli.node_info()
@@ -38,20 +39,21 @@ def transfer(cli: client.Client, sender: local_account.LocalAccount, payee: str,
 
 
 if __name__ == "__main__":
-    cli = client.Client("http://barnard1.seed.starcoin.org:9850")
+    cli = client.Client("http://barnard.seed.starcoin.org:9850")
     # sender
     private_key = Ed25519PrivateKey.from_private_bytes(bytes.fromhex(
         "e424e16db235e3f3b9ef2475516c51d4c15aa5287ceb364213698bd551eab4f2"))
     sender = local_account.LocalAccount(private_key)
 
     # reciver
-    payee_public_key_hex = "a1d2f3e34563ef6e63e0d01e91bd53e9d27da758b25b01771572c240cb4f3113"
+    payee_public_key_hex = "848a4e958af8f127a0b4797ab0a2868e2e15b5232ed1076fdf34228433f96a2f"
     pk = Ed25519PublicKey.from_public_bytes(
         bytes.fromhex(payee_public_key_hex))
     payee_auth_key = auth_key.AuthKey.from_public_key(pk)
     payee_address = payee_auth_key.account_address()
     if cli.is_account_exist("0x"+payee_address.bcs_serialize().hex()):
         payee_auth_key = auth_key.AuthKey(b"")
-    transfer(cli, sender, payee_address, 1024, payee_auth_key.data)
+    receipt = ReceiptIdentifier(payee_address, payee_auth_key).encode()
+    transfer(cli, sender, receipt, 1024)
     print(cli.get_account_token(
         utils.account_address_hex(payee_address), "STC", "STC"))
